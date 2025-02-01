66a2e5ee2625001d0334472a6b4e955ad264f028
CAN <linpyae5112003@gmail.com>
2025-02-02 02:03:44 +0630

    homePage

---
+++ assets/sr.js
@@ -0,0 +1,40 @@
+ScrollReveal().reveal('.infoOne',
+    {
+    delay:200,
+    distance:'100px',
+    // reset:true,
+    origin:'left',
+    interval:300,
+    duration:1000
+    }
+);
+ScrollReveal().reveal('.infoTwo',
+    {
+    delay:200,
+    distance:'100px',
+    // reset:true,
+    origin:'right',
+    interval:500,
+    duration:1000
+    }
+);
+ScrollReveal().reveal('.infoThree',
+    {
+    delay:200,
+    distance:'100px',
+    // reset:true,
+    origin:'left',
+    interval:300,
+    duration:1000
+    }
+);
+ScrollReveal().reveal('.infoFour',
+    {
+    delay:200,
+    distance:'100px',
+    // reset:true,
+    origin:'right',
+    interval:500,
+    duration:1000
+    }
+);
